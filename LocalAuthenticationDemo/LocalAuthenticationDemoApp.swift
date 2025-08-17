dca51c71594bd4ce8f2bea2714b2ba67e02b4886
itsuki <itsuki.enjyoy@gmail.com>
2025-08-17 10:48:44 +0900

    Initial Commit

---
+++ LocalAuthenticationDemo/LocalAuthenticationDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  LocalAuthenticationDemoApp.swift
+//  LocalAuthenticationDemo
+//
+//  Created by Itsuki on 2025/08/17.
+//
+
+import SwiftUI
+
+@main
+struct LocalAuthenticationDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
